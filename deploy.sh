cfed82546fccb5f6d810472cd8841bd11070f16c
Li <1542902425.@QQ.com>
2024-09-12 11:49:10 +0800

    update

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+#  echo 'doc.frontdoc.cn' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:LiPanFQ/eLinks_Doc.github.io.git master
+
+cd -
