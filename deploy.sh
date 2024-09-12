0ba75a3cfb4c0d8a4b8437aa494f6ed0a0bee2e4
Li <1542902425.@QQ.com>
2024-09-12 17:36:49 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # 生成静态文件
-# npm run build:win
+npm run build:win
 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 #  echo 'doc.frontdoc.cn' > CNAME
@@ -15,6 +15,5 @@ set -e
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:LiPanFQ/eLinks_Doc.github.io.git master
-
+git push -f https://lipanfq.github.io/eLinks_Doc.github.io/ gh-pages
 cd -
